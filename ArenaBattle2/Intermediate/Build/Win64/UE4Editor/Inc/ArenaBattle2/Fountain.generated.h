7b57dad72b245243f4ec20de998552c42b3f1c8f
sinyoung <hsykpu@gmail.com>
2023-05-03 22:12:51 +0900

    프로젝트 생성 및 예제 환경 세팅

---
+++ ArenaBattle2/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle2/Fountain.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARENABATTLE2_Fountain_generated_h
+#error "Fountain.generated.h already included, missing '#pragma once' in Fountain.h"
+#endif
+#define ARENABATTLE2_Fountain_generated_h
+
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_SPARSE_DATA
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_RPC_WRAPPERS
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFountain(); \
+	friend struct Z_Construct_UClass_AFountain_Statics; \
+public: \
+	DECLARE_CLASS(AFountain, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle2"), NO_API) \
+	DECLARE_SERIALIZER(AFountain)
+
+
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAFountain(); \
+	friend struct Z_Construct_UClass_AFountain_Statics; \
+public: \
+	DECLARE_CLASS(AFountain, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle2"), NO_API) \
+	DECLARE_SERIALIZER(AFountain)
+
+
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFountain(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFountain) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFountain); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFountain); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFountain(AFountain&&); \
+	NO_API AFountain(const AFountain&); \
+public:
+
+
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFountain(AFountain&&); \
+	NO_API AFountain(const AFountain&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFountain); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFountain); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AFountain)
+
+
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_PRIVATE_PROPERTY_OFFSET
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_9_PROLOG
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_SPARSE_DATA \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_RPC_WRAPPERS \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_INCLASS \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_SPARSE_DATA \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_INCLASS_NO_PURE_DECLS \
+	ArenaBattle2_Source_ArenaBattle2_Fountain_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARENABATTLE2_API UClass* StaticClass<class AFountain>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ArenaBattle2_Source_ArenaBattle2_Fountain_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
